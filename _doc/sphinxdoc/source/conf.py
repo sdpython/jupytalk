b025746c8a7a7b0905329e133842bf4ead486c53
dupre <xavier.dupre@ensae.fr>
2016-06-20 23:53:44 +0200

    change theme

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -3,7 +3,7 @@ import sys
 import os
 import datetime
 import re
-import foundation_sphinx_theme
+import sphinx_readable_theme
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0])))
 sys.path.insert(
@@ -20,18 +20,9 @@ sys.path.insert(
 
 from pyquickhelper.helpgen.default_conf import set_sphinx_variables
 set_sphinx_variables(__file__, "jupytalk", "Xavier Dupré", 2016,
-                     "foundation_sphinx_theme", None, locals(),
-                     extlinks=dict(
+                     "readable", [sphinx_readable_theme.get_html_theme_path()],
+                     locals(), extlinks=dict(
                          issue=('https://github.com/sdpython/jupytalk/issues/%s', 'issue')),
                      github_user="sdpython", github_repo="jupytalk")
 
 blog_root = "http://www.xavierdupre.fr/app/jupytalk/helpsphinx/"
-html_theme_options["top_bar_force_fit"] = True
-
-# https://pypi.python.org/pypi/hbp-sphinx-theme/0.3.4
-# https://pypi.python.org/pypi/oslosphinx/3.2.0
-# https://pypi.python.org/pypi/foundation-sphinx-theme/0.0.3
-# https://pypi.python.org/pypi/pietroalbini-sphinx-themes/1.3.0
-# https://github.com/lucywyman/colorful-hieroglyph-theme
-# https://pypi.python.org/pypi/openstackdocstheme/1.3.0
-# https://pypi.python.org/pypi/sphinx_theme_pd/0.0.8
