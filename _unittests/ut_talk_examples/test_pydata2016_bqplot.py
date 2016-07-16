3454e113c614bc964bc97cf9578485cacd7c5f6f
dupre <xavier.dupre@ensae.fr>
2016-07-16 12:39:48 +0200

    update bqplot to fix an issue with a json file after an update

---
+++ _unittests/ut_talk_examples/test_pydata2016_bqplot.py
@@ -0,0 +1,64 @@
+"""
+@brief      test log(time=2s)
+"""
+
+import sys
+import os
+import unittest
+import bqplot
+
+
+try:
+    import src
+except ImportError:
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..")))
+    if path not in sys.path:
+        sys.path.append(path)
+    import src
+
+try:
+    import pyquickhelper as skip_
+except ImportError:
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..",
+                "..",
+                "pyquickhelper",
+                "src")))
+    if path not in sys.path:
+        sys.path.append(path)
+    import pyquickhelper as skip_
+
+from pyquickhelper.loghelper import fLOG
+from pyquickhelper.filehelper import get_url_content_timeout
+from pyquickhelper.pycode import get_temp_folder
+from bqplot import topo_load
+
+
+class TestPyData2016bqplot(unittest.TestCase):
+
+    def test_bqplot_topo_load(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        temp = get_temp_folder(__file__, "temp_bqplot_topo_load")
+        f = 'WorldMapData.json'
+        full = os.path.join(temp, f)
+        url = "https://raw.githubusercontent.com/bloomberg/bqplot/master/bqplot/map_data/"
+        get_url_content_timeout(url + f, output=full)
+        r = topo_load(full)
+        assert r
+
+
+if __name__ == "__main__":
+    unittest.main()
