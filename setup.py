0a00e2acda52db31bfa56bb2babd47609f82e89a
xavier dupré <xavier.dupre@gmail.com>
2017-08-08 01:59:22 +0200

    licence

--- setup.py
+++ setup.py
@@ -235,6 +235,7 @@ if not r:
         version='%s%s' % (sversion, subversion),
         author='Xavier Dupré',
         author_email='xavier.dupre@gmail.com',
+        license="MIT",
         url="http://www.xavierdupre.fr/app/jupytalk/helpsphinx/index.html",
         download_url="https://github.com/sdpython/jupytalk",
         description=DESCRIPTION,
