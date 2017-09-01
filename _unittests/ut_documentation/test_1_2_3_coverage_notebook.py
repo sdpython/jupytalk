d860060407a1ee46fa2b4c69514fc8de6b0acaa3
xavier dupré <xavier.dupre@gmail.com>
2017-09-01 23:01:51 +0200

    increase notebook coverage

---
+++ _unittests/ut_documentation/test_1_2_3_coverage_notebook.py
@@ -0,0 +1,124 @@
+#-*- coding: utf-8 -*-
+"""
+@brief      test log(time=21s)
+"""
+
+import sys
+import os
+import unittest
+
+
+try:
+    import src
+except ImportError:
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..")))
+    if path not in sys.path:
+        sys.path.append(path)
+    import src
+
+try:
+    import pyquickhelper as skip_
+except ImportError:
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..",
+                "..",
+                "pyquickhelper",
+                "src")))
+    if path not in sys.path:
+        sys.path.append(path)
+    import pyquickhelper as skip_
+
+from pyquickhelper.loghelper import fLOG
+from pyquickhelper.pycode import get_temp_folder, add_missing_development_version
+from pyquickhelper.ipythonhelper import execute_notebook_list, execute_notebook_list_finalize_ut, get_additional_paths
+
+
+class TestNotebook123Coverage(unittest.TestCase):
+
+    def setUp(self):
+        add_missing_development_version(["pymyinstall", "pyensae", "pymmails", "jyquickhelper"],
+                                        __file__, hide=True)
+
+    def a_test_notebook_runner(self, name, folder, valid=None):
+        import src.jupytalk
+        temp = get_temp_folder(__file__, "temp_notebook_123_{0}".format(name))
+        doc =  os.path.join(temp, "..", "..", "..", "_doc", "notebooks", folder)
+        self.assertTrue(os.path.exists(doc))
+        keepnote = [os.path.join(doc, _) for _ in os.listdir(doc) if name in _]
+        self.assertTrue(len(keepnote) > 0)
+
+        import pyquickhelper
+        import jyquickhelper
+        import pyensae
+        import src.jupytalk
+        add_path = get_additional_paths([jyquickhelper, pyquickhelper, pyensae, src.jupytalk])
+        res = execute_notebook_list(temp, keepnote, additional_path=add_path, valid=valid)
+        execute_notebook_list_finalize_ut(res, fLOG=fLOG, dump=src.jupytalk)                          
+
+    def test_notebook_automation_finance_trading(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        self.a_test_notebook_runner("automation_finance_trading", "2017/meshs")
+
+    def test_notebook_centrale_201606_sysrec(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        self.a_test_notebook_runner("centrale_201606_sysrec", "2016/centrale")
+
+    def test_notebook_kaggle_review_2016(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        self.a_test_notebook_runner("kaggle_review_2016", "2016/ensae")
+
+    def test_notebook_10_plotting_libraries(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        self.a_test_notebook_runner("10_plotting_libraries", "2016/pydata")
+
+    def test_notebook_js_lightning_python(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+            
+        def valid(cell):
+            if "from lightning import Lightning" in cell:
+                return False
+            if "lgn.scatter(x, y, alpha=0.5, values=v, colormap='Reds')" in cell:
+                return False
+            return True
+
+        self.a_test_notebook_runner("js_lightning_python", "2016/pydata", valid=valid)
+
+    def test_notebook_pyjsc_vispy(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        self.a_test_notebook_runner("pyjsc_vispy", "2016/pydata")
+
+
+if __name__ == "__main__":
+    unittest.main()
