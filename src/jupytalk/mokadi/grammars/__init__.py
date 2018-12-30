69cb9d19dceb54e4c6b97e35e61ce295d374f23c
xavier dupré <xavier.dupre@gmail.com>
2018-12-30 12:06:05 +0100

    Fix #14, move chatbot somewhere else

--- src/jupytalk/mokadi/grammars/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-@file
-@brief Shorcuts to grammar.
-"""
-
-from .MokadiGrammar_frLexer import MokadiGrammar_frLexer
-from .MokadiGrammar_frListener import MokadiGrammar_frListener
-from .MokadiGrammar_frParser import MokadiGrammar_frParser
