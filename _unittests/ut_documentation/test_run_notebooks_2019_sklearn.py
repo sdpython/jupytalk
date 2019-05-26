6bdf94e3d47061b6a278b123c1aa235448b36877
xavier dupré <xavier.dupre@gmail.com>
2019-05-26 13:33:19 +0200

    add unit test

--- _unittests/ut_documentation/test_run_notebooks_2019_sklearn.py
+++ _unittests/ut_documentation/test_run_notebooks_2019_sklearn.py
@@ -1,38 +1,33 @@
 # -*- coding: utf-8 -*-
 """
-@brief      test log(time=7s)
+@brief      test log(time=22s)
 """
 
 import sys
 import os
 import unittest
+import shutil
 from pyquickhelper.loghelper import fLOG
-from pyquickhelper.pycode import get_temp_folder
+from pyquickhelper.pycode import get_temp_folder, is_travis_or_appveyor
 from pyquickhelper.ipythonhelper import test_notebook_execution_coverage
 import jupytalk
 import jyquickhelper
 
 
-class TestRunNotebooksPyData2016_gui(unittest.TestCase):
+class TestRunNotebooks2019_sklearn(unittest.TestCase):
 
-    def test_run_notebook_gui(self):
+    def test_notebook_sklearn(self):
         fLOG(
             __file__,
             self._testMethodName,
             OutputPrint=__name__ == "__main__")
 
-        def valid(cell):
-            if ".show" in cell:
-                return False
-            return True
-
         self.assertTrue(jupytalk is not None)
         self.assertTrue(jyquickhelper is not None)
         folder = os.path.join(os.path.dirname(__file__),
-                              "..", "..", "_doc", "notebooks", "2016", "pydata")
+                              "..", "..", "_doc", "notebooks", "2019", "sklearn")
         test_notebook_execution_coverage(
-            __file__, "gui", folder, 'jupytalk', copy_files=[], fLOG=fLOG,
-            valid=valid)
+            __file__, "sklearn", folder, 'jupytalk', copy_files=[], fLOG=fLOG)
 
 
 if __name__ == "__main__":
