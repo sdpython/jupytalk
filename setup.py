f384d0d0ffd80132d8157b8787757b2abc46b42f
xavier dupré <xavier.dupre@gmail.com>
2019-11-10 19:13:08 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -137,7 +137,7 @@ if is_local():
         fLOG=logging_function, covtoken=(
             "989a8320-d21b-47f4-910b-f1fd9b2e5415", "'_UT_37_std' in outfile"),
         nbformats=('ipynb', 'html', 'python', 'rst',
-                   'slides', 'present', 'github'),
+                   'slides', 'github'),
         github_owner='sdpython')
     if not r and "update_grammars" in sys.argv:
         # expecting python setup.py update_grammars file
