4102d9edc8fb0d238b3bbe3cb050fcfa24591a22
xavier dupré <xavier.dupre@gmail.com>
2022-07-28 00:03:32 +0200

    Update test_code_style.py

--- _unittests/ut_module/test_code_style.py
+++ _unittests/ut_module/test_code_style.py
@@ -21,7 +21,7 @@ class TestCodeStyle(ExtTestCase):
         thi = os.path.abspath(os.path.dirname(__file__))
         src_ = os.path.normpath(os.path.join(thi, "..", "..", "src"))
         check_pep8(src_, fLOG=fLOG, run_cmd_filter=_run_cmd_filter,
-                   pylint_ignore=('C0103', 'C1801', 'R0201', 'R1705', 'W0108', 'W0613',
+                   pylint_ignore=('C0103', 'C1801', 'R1705', 'W0108', 'W0613',
                                   'R0911', 'W0201', 'W070', 'W0622', 'R1702',
                                   'C0111', 'W0703', 'C0200', 'C0415', 'C0209'),
                    neg_pattern='.*MokadiGrammar_((frParser)|(frListener)|(frLexer))[.]py$',
@@ -53,8 +53,8 @@ class TestCodeStyle(ExtTestCase):
         thi = os.path.abspath(os.path.dirname(__file__))
         test = os.path.normpath(os.path.join(thi, "..", ))
         check_pep8(test, fLOG=fLOG, neg_pattern="temp_.*", run_cmd_filter=_run_cmd_filter,
-                   pylint_ignore=('C0103', 'C1801', 'R0201', 'R1705', 'W0108', 'W0613',
-                                  'C0111', 'W0703', 'C0122', 'W0101', 'R1707', 'C0330',
+                   pylint_ignore=('C0103', 'C1801', 'R1705', 'W0108', 'W0613',
+                                  'C0111', 'W0703', 'C0122', 'W0101', 'R1707',
                                   'W0611', 'C0415', 'C0209'),
                    skip=["Unable to import 'ensae_teaching_cs.pythonnet'",
                          "Redefining name 'path' from outer scope",
